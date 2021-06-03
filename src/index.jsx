aa4c195e8db4ba2e85c5b3e0250f46846ed01dee
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-06-02 23:03:07 -0700

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import statement to indicate that you need to bundle `./index.scss`
+import './index.scss';
+
+// Main component
+class myFlixApplication extends React.Component {
+  render() {
+    return (
+      <div className="my-flix">
+        <div>Good Morning</div>
+      </div>
+    );
+  }
+}
+
+//Find root of app
+const container = document.getElementByClassName('app-container')[0];
+
+//Render app in the root DOM element
+ReactDOM.render(React.createElement(myFlixApplication), container);
