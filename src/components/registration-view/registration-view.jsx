640bc61d167b0b4e6312806cb98db9df385ad49d
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-06-28 23:01:23 -0700

    create registration form

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -13,3 +13,26 @@ export function RegistrationView(props) {
     console.log(username, password, email, birthday);
     props.onRegistration(username);
   };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Birthday:
+        <input type="date" value={birthday} onChage={e => setBirthday(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
