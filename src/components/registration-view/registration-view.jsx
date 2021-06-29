c591167c92816f1546a5664d558427ce6efe19dd
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-06-28 23:01:11 -0700

    add const variables

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,2 +1,15 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+
+export function RegistrationView(props) {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [birthday, setBirthday] = useState('');
+
+  const handleSubmit = () => {
+    e.preventDefault();
+    console.log(username, password, email, birthday);
+    props.onRegistration(username);
+  };
