186dc4a78c5162b762c917e462b99f88fd9e624c
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-08-24 21:38:15 -0700

    import views

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,6 +9,9 @@ import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { ProfileView } from '../profile-view/profile-view';
+import { UpdateView } from '../update-view/update-view';
+import { NavBarView } from '../navbar-view/navbar-view';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
