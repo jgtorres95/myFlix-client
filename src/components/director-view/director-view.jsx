7b0e0a05f51609254189d85592153b8f8d5d172c
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-08-24 21:37:27 -0700

    create director view

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import './director-view.scss';
+
+export class DirectorView extends React.Component {
+  render() {
+    const { director, onBackClick } = this.props;
+
+    return (
+      <Container>
+        <Row>
+          <h1>{director.Name}</h1>
+        </Row>
+        <Row>
+          <p>{director.Bio}</p>
+        </Row>
+        <Row>
+          <Button variant="dark" onClick={() => { onBackClick(null); }}>Back</Button>
+        </Row>
+      </Container>
+    )
+  }
+}
