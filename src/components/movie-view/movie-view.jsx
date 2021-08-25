3ea477e88022a39931970a51572d8e9bcc70665e
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-08-24 21:41:32 -0700

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button'
-import './movie-view.scss'
+import Button from 'react-bootstrap/Button';
+import './movie-view.scss';
 
 import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
