c5569c9eea3b69d802e91da309c3fd7fae757b36
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-10-13 11:06:23 -0700

    add comments

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -5,18 +5,22 @@ import { connect } from 'react-redux';
 import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
 import { MovieCard } from '../movie-card/movie-card';
 
+// extract visibilityFilter state from store
 const mapStateToProps = state => {
   const { visibilityFilter } = state;
   return { visibilityFilter };
 };
 
+// create MoviesList component 
 function MoviesList(props) {
   const { movies, visibilityFilter } = props;
   let filteredMovies = movies;
 
+  // filter movies based on user input
   if (visibilityFilter !== '') {
     filteredMovies = movies.filter(m => m.Title.toLowerCase().includes(visibilityFilter.toLowerCase()));
   }
+
   if (!movies) return <div className="main-view" />;
 
   return <>
