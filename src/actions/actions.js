60d5bffe65efdf01111cbfe8d6eb352ab903e47a
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2021-10-13 11:05:02 -0700

    add comments

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,9 @@
+// create action types
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 export const SET_USER = 'SET_USER';
 
+//create action creators
 export function setMovies(value) {
   return { type: SET_MOVIES, value};
 }
